1f5f5edc0e80ce64b7bcf45b00660824226b6ec5
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-05-05 11:04:40 +0000

    feat: add args module and update image dependency for PNG conversion

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
+pub mod args;
+
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+pub fn to_png(image_bytes: &[u8]) -> Result<Vec<u8>, JsError> {
+    let img = image::load_from_memory(image_bytes).map_err(|e| JsError::from(e))?;
+
+    let mut output = std::io::Cursor::new(Vec::new());
+
+    img.write_to(&mut output, image::ImageFormat::Png)
+        .map_err(|e| JsError::from(e))?;
+
+    Ok(output.into_inner())
 }
