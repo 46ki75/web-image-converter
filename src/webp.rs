d6f7f39cd4248829c47b7afd0e43125d96283c7d
Ikuma Yamashita <me@ikuma.cloud>
2026-03-25 07:20:39 +0900

    feat: implement WebP encoding with quality option and update dependencies

--- src/webp.rs
+++ src/webp.rs
@@ -1,5 +1,6 @@
 use image::ImageReader;
 use wasm_bindgen::prelude::*;
+use webp::{Encoder, WebPMemory};
 
 #[wasm_bindgen]
 pub fn webp() -> WebpOption {
@@ -8,7 +9,9 @@ pub fn webp() -> WebpOption {
 
 #[wasm_bindgen]
 #[derive(Debug, Clone, Copy, Default)]
-pub struct WebpOption {}
+pub struct WebpOption {
+    quality: Option<f32>,
+}
 
 #[wasm_bindgen]
 impl WebpOption {
@@ -22,12 +25,24 @@ impl WebpOption {
             .with_guessed_format()?
             .decode()?;
 
-        let mut bytes = Vec::new();
+        if let Some(quality) = self.quality {
+            if quality < 1f32 || quality > 100f32 {
+                return Err(JsError::new("quality must be between 1 and 100"));
+            }
 
-        let encoder = image::codecs::webp::WebPEncoder::new_lossless(&mut bytes);
+            let encoder: Encoder = Encoder::from_image(&img).unwrap();
 
-        img.write_with_encoder(encoder)?;
+            let webp: WebPMemory = encoder.encode(quality);
 
-        Ok(bytes)
+            Ok(webp.to_vec())
+        } else {
+            let mut bytes = Vec::new();
+
+            let encoder = image::codecs::webp::WebPEncoder::new_lossless(&mut bytes);
+
+            img.write_with_encoder(encoder)?;
+
+            Ok(bytes)
+        }
     }
 }
